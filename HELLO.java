e3a96328048067672e95e2dc62020e077ee8e846
mgy91 <magendrarao102@gmail.com>
2026-05-24 13:48:33 +0530

    Day 1: Started Java

---
+++ HELLO.java
@@ -0,0 +1,14 @@
+
+public class HELLO{
+    public static void main(String[] args) {
+
+        int num1 = 3;
+        int num2 = 4;
+        int result = num1 + num2;
+
+        System.out.println(result);
+    }
+
+}
+
+
